a77dfd862aa1892ea538166a8f2ad061f43cf47a
Olmeiro orozco <olme.orozco@gmail.com>
2022-08-26 18:13:31 -0500

    config useReducer state

--- 3-todo-react-typescript/src/todo/context/TodoProvider.tsx
+++ 3-todo-react-typescript/src/todo/context/TodoProvider.tsx
@@ -1,4 +1,26 @@
+import { useReducer } from "react"
+import { TodoState } from "../interfaces/interfaces"
 import { TodoContext } from "./TodoContext"
+import { todoReducer } from "./TodoReducer"
+
+// ctrl + . sobre el error para completar
+const INITIAL_STATE: TodoState = {
+  todoCount: 2,
+  todos: [
+    {
+      id: '1',
+      desc: 'Aprender TS',
+      completed: false
+    },
+    {
+      id: '2',
+      desc: 'Aprender React Native',
+      completed: false
+    },
+  ],
+  completed: 0,
+  pending: 2
+}
 
 // como lucen las props que recibe TodoProvider:
 interface props {
@@ -6,8 +28,11 @@ interface props {
 }
 
 export const TodoProvider = ({ children }:props) => {
+
+  const [todoState, dispatch] = useReducer(todoReducer, INITIAL_STATE)
+
   return (
-    <TodoContext.Provider value={{}}>
+    <TodoContext.Provider value={{ todoState }}>
       { children }
     </TodoContext.Provider>
   )
---
+++ 3-todo-react-typescript/src/todo/context/todoReducer.ts
@@ -0,0 +1,24 @@
+import { TodoState, Todo } from '../interfaces/interfaces';
+
+type TodoAction = 
+  | { type: 'addTodo', payload: Todo }
+  | { type: 'toggleTodo', payload: {id: string} }
+
+export const todoReducer = (state: TodoState, action:TodoAction ): TodoState => {
+  switch (action.type) {
+    case 'addTodo':
+      return {
+        ...state,
+        todos: [ ...state.todos, action.payload ]
+      }
+    case 'toggleTodo':
+      return {
+        ...state,
+        
+      }
+    
+  
+    default:
+      return state
+  }
+}
