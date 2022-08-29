78bf0aff3d602dde70acf0458e3bf7e9c2d5f71d
Olmeiro orozco <olme.orozco@gmail.com>
2022-08-29 10:34:47 -0500

    feature 2: one cat more -  without using objects

--- 4-Javascript-patterns/1-cat-clicker/main.js
+++ 4-Javascript-patterns/1-cat-clicker/main.js
@@ -1,16 +1,59 @@
 // my code
-const little_cat = document.getElementById('little-cat');
-const spanClick = document.getElementById('clicks')
 
-let count = 0;
+const nameCats = ['Dubby', 'Chal'];
+const imagesCats = ['./assets/cat.jpg', './assets/cat2.jpg', './assets/cat3.jpg']
 
-little_cat.addEventListener('click', () => {
-  increment()
+const Cats = [
+  {
+    name: 'Dubby',
+    image: './assets/cat.jpg'
+  },
+  {
+    name: 'Chal',
+    image: './assets/cat2.jpg'
+  }
+
+]
+
+const essay = document.getElementById('essay')
+essay.src = imagesCats[2]
+
+const catImages = document.querySelectorAll('img')
+const catArray= Array.from(catImages)
+
+const cat1 = document.getElementById('cat-1')
+const cat2 = document.getElementById('cat-2')
+
+const spanClickCat1 = document.getElementById('span-cat1')
+const spanClickCat2 = document.getElementById('span-cat2')
+
+const spanName = document.querySelectorAll('.name_cat')
+const arraySpanNames = Array.from(spanName)
+
+arraySpanNames.forEach((cat,idx) => {
+  cat.innerHTML = nameCats[idx]
 })
 
-function increment(){
-  count = count + 1;
-  spanClick.innerHTML = count
+let countCat1 = 0;
+let countCat2 = 0;
+
+cat1.addEventListener('click', () => {
+  incrementCat1()
+})
+
+cat2.addEventListener('click', () => {
+  incrementCat2()
+})
+
+
+function incrementCat1(){
+  countCat1 = countCat1 + 1
+  spanClickCat1.innerHTML = countCat1 + 1
+}
+
+function incrementCat2(){
+  countCat2 = countCat2 + 1
+  spanClickCat2.innerHTML = countCat2 + 1
 }
 
 
