8160e55987b5e72a2cf7675b59f05bd98e3525ad
Olmeiro orozco <olme.orozco@gmail.com>
2022-08-29 13:49:00 -0500

    add photos cats

--- 4-Javascript-patterns/1-cat-clicker/main.js
+++ 4-Javascript-patterns/1-cat-clicker/main.js
@@ -1,59 +1,86 @@
 // my code
 
-const nameCats = ['Dubby', 'Chal'];
-const imagesCats = ['./assets/cat.jpg', './assets/cat2.jpg', './assets/cat3.jpg']
+const nameCats = ["Dubby", "Chal"];
+const imagesCats = [
+  "./assets/cat.jpg",
+  "./assets/cat2.jpg",
+  "./assets/cat3.jpg",
+];
 
 const Cats = [
   {
-    name: 'Dubby',
-    image: './assets/cat.jpg'
+    name: "Dubby",
+    image: "./assets/cat.jpg",
   },
   {
-    name: 'Chal',
-    image: './assets/cat2.jpg'
-  }
+    name: "Chal",
+    image: "./assets/cat2.jpg",
+  },
+  {
+    name: "Bum",
+    image: "./assets/cat3.jpg",
+  },
+];
 
-]
+{
+  /* <div>
+        <h2>Cat Name:
+          <span class="name_cat">name1</span>
+        </h2>
+        <img id="cat-1" src="./assets/cat.jpg" alt="">
+        <p>number of clicks:</p>
+        <span class="span-cat1"></span>
+      </div> */
+}
 
-const essay = document.getElementById('essay')
-essay.src = imagesCats[2]
+const main = document.getElementById("root");
 
-const catImages = document.querySelectorAll('img')
-const catArray= Array.from(catImages)
+Cats.forEach((cat, idx) => {
+  const { name, image } = cat;
 
-const cat1 = document.getElementById('cat-1')
-const cat2 = document.getElementById('cat-2')
+  const div = document.createElement("div");
 
-const spanClickCat1 = document.getElementById('span-cat1')
-const spanClickCat2 = document.getElementById('span-cat2')
+  const title = document.createElement("h2");
+  title.textContent = "NAME:";
+  const spanTitle = document.createElement("span");
+  spanTitle.textContent = name;
+  spanTitle.className = "name_cat";
+  title.append(spanTitle);
 
-const spanName = document.querySelectorAll('.name_cat')
-const arraySpanNames = Array.from(spanName)
+  const imageCat = document.createElement("img");
+  imageCat.src = image;
+  imageCat.alt = "cat click app images";
+  imageCat.id = `cat-${idx + 1}`;
 
-arraySpanNames.forEach((cat,idx) => {
-  cat.innerHTML = nameCats[idx]
-})
+  const paragraphClics = document.createElement("p");
+  paragraphClics.textContent = "number of clicks:";
+  paragraphClics.style.fontWeight = 600;
+
+  const spanClicks = document.createElement("span");
+  spanClicks.className = `span-cat${idx + 1}`;
+  spanClicks.textContent = 0;
+
+  paragraphClics.append(spanClicks);
+
+  div.appendChild(title);
+  div.appendChild(imageCat);
+  div.appendChild(paragraphClics);
+  div.appendChild(paragraphClics);
+  div.appendChild(spanClicks);
+
+  main.append(div);
+});
 
 let countCat1 = 0;
 let countCat2 = 0;
 
-cat1.addEventListener('click', () => {
-  incrementCat1()
-})
+const catImages = document.querySelectorAll("img");
 
-cat2.addEventListener('click', () => {
-  incrementCat2()
-})
-
-
-function incrementCat1(){
-  countCat1 = countCat1 + 1
-  spanClickCat1.innerHTML = countCat1 + 1
-}
-
-function incrementCat2(){
-  countCat2 = countCat2 + 1
-  spanClickCat2.innerHTML = countCat2 + 1
-}
-
-
+catImages.forEach((cat, idx) => {
+  cat.addEventListener("click", (e) => {
+    const spanToShowClicks = document.querySelector(`.span-cat${idx + 1}`);
+    let countClicks = parseInt(spanToShowClicks.innerHTML);
+    countClicks = countClicks + 1;
+    spanToShowClicks.textContent = countClicks;
+  });
+});
