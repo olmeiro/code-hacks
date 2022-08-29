560fff6db5879c11603247993f8c33c639d92d89
Olmeiro orozco <olme.orozco@gmail.com>
2022-08-29 09:06:01 -0500

    button 3d

--- 1-react-tailwindcss/src/App.jsx
+++ 1-react-tailwindcss/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       {/* <GridTailwindDesign /> */}
       {/* <ResponsiveTailwind /> */}
       {/* <Button3D /> */}
-      <ToggleButton />
+      {/* <ToggleButton /> */}
     </>
   );
 }
