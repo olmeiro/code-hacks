8ee906855a33e770e7de34c25b3000a2dcb13540
Olmeiro orozco <olme.orozco@gmail.com>
2022-09-16 12:06:27 -0500

    inicio product component pattern

--- 8-compound-component-pattern/src/component-patterns/components/ProductCard.tsx
+++ 8-compound-component-pattern/src/component-patterns/components/ProductCard.tsx
@@ -14,22 +14,49 @@ interface Product  {
   img?: string
 }
 
+// components
+
+export const ProductImage = ({img = ''}) => { // el string hace la img opcional-un string vacio para un ternario no tiene valor
+  return (
+    <img className={styles.productImg} src={img ? img : notImage} alt='Product' />
+  )
+}
+
+export const ProductTitle = ({ title}:{title:string}) => { // se obliga el titulo, si hay mas propiedades uso una interface
+  return (
+    <span className={styles.productDescription}>{title}</span>
+  )
+}
+
+interface ProductButtonsProps {
+  counter: number,
+  increaseBy: (value: number) => void
+}
+
+export const ProductButtons = ({counter, increaseBy}: ProductButtonsProps) => {
+  return (
+    <div className={styles.buttonsContainer}>
+      <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+        -
+      </button>
+      <div className={styles.countLabel}>{counter}</div>
+      <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
+        +
+      </button>
+    </div>
+  )
+}
+
 export const ProductCard = ({ product }: Props) => {
   const { counter, increaseBy } = useProduct()
   return (
     <div className={styles.productCard}>
-      <img className={styles.productImg} src={product.img ? product.img : notImage} />
-      <span className={styles.productDescription}>{product.title}</span>
-
-      <div className={styles.buttonsContainer}>
-        <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
-          -
-        </button>
-        <div className={styles.countLabel}>{counter}</div>
-        <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
-          +
-        </button>
-      </div>
+      <ProductImage img={product.img} />
+      <ProductTitle title={product.title} />
+      <ProductButtons 
+        counter={counter} 
+        increaseBy={increaseBy}       
+      />
     </div>
   );
 };
